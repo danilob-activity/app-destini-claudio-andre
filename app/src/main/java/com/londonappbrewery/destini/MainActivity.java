7c7cbb67e10293cd6d5cf61672182baa91325ff2
Cláudio André <andretrafalgar@gmail.com>
2019-05-29 12:00:06 -0300

    1ºcommit

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,8 +1,7 @@
 package com.londonappbrewery.destini;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,6 +12,21 @@ public class MainActivity extends AppCompatActivity {
     Button mAnswerTop;
     Button mAnswerBottom;
     // TODO: Declare as variaveis aqui:
+    Story mT1 =  new Story(R.string.T1_Story);
+    Story mT2 =  new Story(R.string.T1_Story);
+    Story mT3 =  new Story(R.string.T1_Story);
+    Story mT4 =  new Story(R.string.T1_Story);
+    Story mT5 =  new Story(R.string.T1_Story);
+    Story mT6 =  new Story(R.string.T1_Story);
+    Answer mT1_Ans = new Answer(R.string.T1_Ans1);
+    Answer mT1_Ans2 = new Answer(R.string.T1_Ans2);
+    Answer mT2_Ans = new Answer(R.string.T2_Ans1);
+    Answer mT2_Ans2 = new Answer(R.string.T2_Ans2);
+    Answer mT3_Ans = new Answer(R.string.T3_Ans1);
+    Answer mT3_Ans2 = new Answer(R.string.T3_Ans2);
+
+
+
 
     //indice corrente da historia
     private Story mStorySelected;
@@ -24,13 +38,55 @@ public class MainActivity extends AppCompatActivity {
 
 
         //TODO: Faça o link do layout com a activity
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mAnswerTop = findViewById(R.id.buttonTop);
+        mAnswerBottom = findViewById(R.id.buttonBottom);
+
 
         //TODO:faça o mapeamento da história
+        mT1.setAnswerTop(mT1_Ans);
+        mT1.setAnswerBottom(mT1_Ans2);
+        mT1_Ans.setChildStory(mT3);
+        mT3.setAnswerTop(mT3_Ans);
+        mT3.setAnswerBottom(mT3_Ans2);
+        mT3_Ans.setChildStory(mT6);
+        mT3_Ans2.setChildStory(mT5);
+        mT1_Ans2.setChildStory(mT2);
+        mT2.setAnswerTop(mT2_Ans);
+        mT2.setAnswerBottom(mT2_Ans2);
+        mT2_Ans.setChildStory(mT3);
+        mT3.setAnswerTop(mT3_Ans);
+        mT3.setAnswerBottom(mT3_Ans2);
+        mT3_Ans.setChildStory(mT6);
+        mT3_Ans2.setChildStory(mT5);
+        mT2_Ans2.setChildStory(mT4);
+
 
         // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
         // do botão setVisibility(View.GONE):
+        /*
+        mAnswerTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mAnswerBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putInt("StoryKey",mStoryIndex);
+    }*/
+    }
 }
